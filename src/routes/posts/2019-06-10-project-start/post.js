babbf8039836c61533dbc4fa5e4091158fd13139
KJ <kaityagalvan@gmail.com>
2019-06-10 19:51:24 -0700

    Add new post 6/10/19

---
+++ src/routes/posts/2019-06-10-project-start/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Project Overview & Requirements Gathering`,
+  tags: ['eda', 'requirements', 'clean architecture'],
+  spoiler: "Overview of our first week on the project.",
+  getContent: () => import('./document.mdx'),
+}
