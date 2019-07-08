a2f7ee1523014da0dcd3997e176345a5efb7ec12
KJ <kaityagalvan@gmail.com>
2019-07-07 20:18:57 -0700

    Scaffold for next two posts

---
+++ src/routes/posts/2019-06-24-api-template/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `API with Domain Driven Design`,
+  tags: ['c#', 'dotnet', 'api', 'pac', 'clean architecture'],
+  spoiler: "Time to model our entities.",
+  getContent: () => import('./document.mdx'),
+}
---
+++ src/routes/posts/2019-07-1-storing-requests/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Custom Command Handling`,
+  tags: ['c#', 'dotnet', 'api', 'pac'],
+  spoiler: "Figuring out custom command handling with PAC.",
+  getContent: () => import('./document.mdx'),
+}
